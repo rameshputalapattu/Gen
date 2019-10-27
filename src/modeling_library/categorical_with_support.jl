98d9df0c1cea7a05018ac4c039c6ffe090489fc2
rameshputalapattu@outlook.com <rameshputalapattu@gmail.com>
2019-10-27 10:41:58 +0530

    added a categorical distribution which returns samples from a vector of support

---
+++ src/modeling_library/categorical_with_support.jl
@@ -0,0 +1,30 @@
+struct CategoricalWithSupport <: Distribution{Int} end
+
+"""
+    categorical(probs::AbstractArray{U, 1}) where {U <: Real}
+
+Given a vector of probabilities `probs` where `sum(probs) = 1`, and a vector of support, sample an element  `support[i]` from the support with probability `probs[i]`.
+"""
+const categorical_with_support = CategoricalWithSupport()
+
+function logpdf(::CategoricalWithSupport, x::Int, probs::AbstractArray{U,1},support) where {U <: Real}
+    log(probs[x])
+end
+
+function logpdf_grad(::CategoricalWithSupport, x::Int, probs::AbstractArray{U,1},support)  where {U <: Real}
+    grad = zeros(length(probs))
+    grad[x] = 1.0 / probs[x]
+    (nothing, grad)
+end
+
+function random(::CategoricalWithSupport, probs::AbstractArray{U,1},support) where {U <: Real}
+    idx = rand(Distributions.Categorical(probs))
+    support[idx]
+end
+
+(::CategoricalWithSupport)(probs,support) = random(CategoricalWithSupport(), probs,support)
+
+has_output_grad(::CategoricalWithSupport) = false
+has_argument_grads(::CategoricalWithSupport) = (true,)
+
+export categorical_with_support
